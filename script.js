f8b4d0b04ef410f94278945c6f9596dfdc41edd2
leander006 <leanderdsilva06@gmail.com>
2024-01-21 11:55:32 +0530

    Updating with ui part

--- script.js
+++ script.js
@@ -2,56 +2,102 @@ $(document).ready(function () {
   const repoList = $("#repo-list");
   const userInfoContainer = $("#user-info");
   const githubContainer = $(".github-link");
-  const defaultUsername = "leander006"; // Replace with your default GitHub username
+  const loadingOverlay = $("#loading-overlay"); // Add loading overlay element
+  let defaultUsername = "leander006";
   let currentPage = 1;
   const reposPerPage = 6;
+  let totalRepoPages = 1;
 
-  // Function to fetch and display user information
+  function showLoadingOverlay() {
+    loadingOverlay.show();
+  }
+
+  function hideLoadingOverlay() {
+    loadingOverlay.hide();
+  }
+
+  // Utility function to parse the "Link" header and extract the last page number
+  function extractLastPage(linkHeader) {
+    const links = linkHeader.split(", ");
+    for (const link of links) {
+      const match = link.match(/<([^>]+)>;\s*rel="last"/);
+      if (match) {
+        const lastPageLink = match[1];
+        const lastPageMatch = lastPageLink.match(/&page=(\d+)/);
+        return lastPageMatch ? parseInt(lastPageMatch[1]) : totalRepoPages;
+      }
+    }
+    return totalRepoPages;
+  }
+
   function fetchUserInfo(username) {
     const apiUrl = `https://api.github.com/users/${username}`;
 
-    $.get(apiUrl, function (userData) {
-      console.log(userData);
-      // Display user information
-      userInfoContainer.empty();
-      // Create a div for the image and append it to the userContainer
-      const imageContainer = $('<div class="user-image-container"></div>');
-      imageContainer.append(
-        `<img src="${userData.avatar_url}" alt="Profile Picture">`
-      );
-      userInfoContainer.append(imageContainer);
+    showLoadingOverlay(); // Show loading overlay before making the request
 
-      // Create a div for the user details and append it to the userContainer
-      const detailsContainer = $('<div class="user-details-container"></div>');
-      detailsContainer.append(`<h1>${userData.name}</h1>`);
-      detailsContainer.append(`<p>${userData.bio}</p>`);
-      if (userData.blog) {
-        detailsContainer.append(
-          `<p><a href="${userData.blog}" target="_blank">Website</a></p>`
+    $.get(apiUrl)
+      .done(function (userData) {
+        hideLoadingOverlay(); // Hide loading overlay on successful response
+
+        userInfoContainer.empty();
+        githubContainer.empty();
+
+        const imageContainer = $('<div class="user-image-container"></div>');
+        imageContainer.append(
+          `<img src="${userData.avatar_url}" alt="Profile Picture">`
         );
-      }
-      userInfoContainer.append(detailsContainer);
-      githubContainer.append(
-        `<p><a class="link" href="${userData.html_url}" target="_blank">${userData.html_url}</a></p>`
-      );
-      // Display current page number
-      $("#current-page").text(currentPage);
-    });
+        userInfoContainer.append(imageContainer);
+
+        const detailsContainer = $(
+          '<div class="user-details-container"></div>'
+        );
+        detailsContainer.append(`<h1>${userData.name}</h1>`);
+        if (userData.bio) {
+          detailsContainer.append(`<p>${userData.bio}</p>`);
+        } else {
+          detailsContainer.append(`<p>No description</p>`);
+        }
+
+        if (userData.location) {
+          detailsContainer.append(`<p>${userData.location}</p>`);
+        }
+        if (userData.blog) {
+          detailsContainer.append(
+            `<p><a href="${userData.blog}" target="_blank">Website</a></p>`
+          );
+        }
+        userInfoContainer.append(detailsContainer);
+        githubContainer.append(
+          `<p><a class="link" href="${userData.html_url}" target="_blank">${userData.html_url}</a></p>`
+        );
+        // Display current page number
+        $("#current-page").text(currentPage);
+      })
+      .fail(function (error) {
+        hideLoadingOverlay(); // Hide loading overlay on error
+        alert("Failed to fetch repositories. Please try again.");
+
+        console.error("Error fetching repositories:", error);
+      });
   }
 
-  // Function to fetch and display repositories
   function fetchRepositories(username, page) {
     const apiUrl = `https://api.github.com/users/${username}/repos?sort=stars&per_page=${reposPerPage}&page=${page}`;
 
-    $.get(apiUrl, function (data, status, xhr) {
+    showLoadingOverlay(); // Show loading overlay before making the request
+
+    $.get(apiUrl).done(function (data, status, xhr) {
+      hideLoadingOverlay(); // Hide loading overlay on successful response
       repoList.empty();
       data.forEach(function (repo) {
         const repoItem = $('<li class="repo-item"></li>');
         repoItem.append(
-          `<h3><a class="link" href="${repo.html_url}" target="_blank">${repo.name}</a></h3>`
+          `<h3><a class="link" id="r" href="${repo.html_url}" target="_blank">${repo.name}</a></h3>`
         );
         if (repo.description) {
           repoItem.append(`<p>${repo.description}</p>`);
+        } else {
+          repoItem.append(`<p>No description</p>`);
         }
 
         // Check if languages information is available
@@ -98,26 +144,54 @@ $(document).ready(function () {
         repoList.append(repoItem);
       });
 
-      // Enable or disable Next and Previous buttons based on the Link header from GitHub API
       const linkHeader = xhr.getResponseHeader("Link");
-      const hasNextPage = linkHeader && linkHeader.includes('rel="next"');
-      const hasPrevPage = linkHeader && linkHeader.includes('rel="prev"');
-      $("#next-page").prop("disabled", !hasNextPage);
-      $("#prev-page").prop("disabled", !hasPrevPage);
+      totalRepoPages = extractLastPage(linkHeader);
+      updatePaginationButtons(xhr);
+      updatePagination(page, totalRepoPages);
     });
+  }
+
+  function updatePaginationButtons(xhr) {
+    const linkHeader = xhr.getResponseHeader("Link");
+    const hasNextPage = linkHeader && linkHeader.includes('rel="next"');
+    const hasPrevPage = linkHeader && linkHeader.includes('rel="prev"');
+    $("#next-page").prop("disabled", !hasNextPage);
+    $("#prev-page").prop("disabled", !hasPrevPage);
+  }
+
+  function updatePagination(currentPage, totalPages) {
+    $("#page-numbers").empty();
+    for (let i = 1; i <= totalPages; i++) {
+      const pageNumberButton = $(
+        `<button class="page-number-button">${i}</button>`
+      );
+      pageNumberButton.click(function () {
+        fetchRepositories(defaultUsername, i);
+      });
+      if (i === currentPage) {
+        pageNumberButton.addClass("current-page");
+      }
+      $("#page-numbers").append(pageNumberButton);
+    }
+
+    $("#current-page").text(currentPage);
   }
 
   // Initial fetch with default username and page 1
   fetchUserInfo(defaultUsername);
   fetchRepositories(defaultUsername, currentPage);
 
-  // Event listener for the search form
-  $("#search-form").submit(function (event) {
+  $("#search-btn").click(function (event) {
     event.preventDefault();
-    const username = $("#search-input").val();
-    currentPage = 1;
-    fetchUserInfo(username);
-    fetchRepositories(username, currentPage);
+
+    const username = $("#username-input").val();
+    $("#username-input").val("");
+    defaultUsername = username.trim(); // Trim leading and trailing spaces
+    if (defaultUsername !== "") {
+      currentPage = 1;
+      fetchUserInfo(defaultUsername);
+      fetchRepositories(defaultUsername, currentPage);
+    }
   });
 
   // Event listener for the search input click
@@ -125,30 +199,13 @@ $(document).ready(function () {
     $(this).val("");
   });
 
-  function updatePaginationButtons(hasNextPage, hasPrevPage) {
-    $("#next-page").prop("disabled", !hasNextPage);
-    $("#prev-page").prop("disabled", !hasPrevPage);
-  }
-
-  function updatePagination(page, totalPages) {
-    $("#page-numbers").empty();
-
-    for (let i = 1; i <= totalPages; i++) {
-      const pageNumberButton = $(
-        `<button class="page-number-button">${i}</button>`
-      );
-      pageNumberButton.click(function () {
-        currentPage = i;
-        const username = $("#search-input").val() || defaultUsername;
-        fetchRepositories(username, currentPage);
-      });
-      $("#page-numbers").append(pageNumberButton);
-    }
-
-    // Highlight the current page
-    $(`#page-numbers button:contains('${page}')`).addClass("current-page");
-  }
-
+  // Event listener for the refreshing
+  $(".github").click(function () {
+    defaultUsername = "leander006";
+    console.log("defaultUsername", defaultUsername);
+    fetchUserInfo(defaultUsername);
+    fetchRepositories(defaultUsername, currentPage);
+  });
   // Event listener for the "Next" button
   $("#next-page").click(function () {
     currentPage++;
